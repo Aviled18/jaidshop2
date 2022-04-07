7375bbf102d672ce600a5a10897653220d7a051d
avinash <adhsolutionltd@gmail.com>
2022-04-07 17:35:56 +0400

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Qualhygiene</a>
+        </header>
+        <main>List of products</main>
+        
+
+    </div>
+  );
+}
+
+export default App;
