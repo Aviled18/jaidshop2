25ec36f179fe382d94e8f4ef5df95ebbc2245829
avinash <adhsolutionltd@gmail.com>
2022-04-15 00:17:44 +0400

    publish 18

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -45,6 +45,7 @@ function ProductScreen() {
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState('');
   const [selectedImage, setSelectedImage] = useState('');
+
   const navigate = useNavigate();  
   const params = useParams();
   const { slug } = params;
@@ -62,7 +63,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
